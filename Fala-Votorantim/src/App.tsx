0f63b56ce243ce6209aaf78c424370f0b067277e
DANIEL DE FREITAS ESTEVES DA COSTA <143741099+BlackDani557@users.noreply.github.com>
2026-09-11 21:03:34 -0300

    Adicionei só os caminhos no app e adicionei um documento que seira a tela inicial do user, que achei que faria sentido ter, fora isso n adicionei mais nada

--- Fala-Votorantim/src/App.tsx
+++ Fala-Votorantim/src/App.tsx
@@ -1,12 +1,58 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+// Páginas inicias
+
+import LadingPage from './Pages/PaginaPrincipal/LadingPage/LadingPage'
+import Sobre from './Pages/PaginaPrincipal/SobreNos/SobreNos'
+import FeedDenuncia from './Pages/PaginaPrincipal/FeedDenuncias/FeedDenuncias'
+import Registro from './Pages/PaginaPrincipal/Registro/Registro'
+
+{/*ADM */}
+
+import TelaInicialADM from './Pages/PaginaADM/TelaInicial/TelaInicial'
+import DashboardADM from './Pages/PaginaADM/Dashboard/Dashboard'
+import FeedDenuncias from './Pages/PaginaADM/FeedDenuncias/FeedDenuncias'
+
+
+{/* Users */}
+
+import TelaInicialUser from './Pages/PaginaUser/TelaInicialUser/TelaInicialUser'
+import MeusDados from './Pages/PaginaUser/MeusDados/MeusDados'
+import Notificacoes from './Pages/PaginaUser/Notificacoes/Notificacoes'
+import MinhasDenuncias from './Pages/PaginaUser/MinhasDenuncias/MinhasDenuncias'
+
+
+
 
 function App() {
 
 
   return (
-    <>
-      oie
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Páginas iniciais */}
+        <Route path='/' element={<LadingPage />}/>
+        <Route path='/Sobre' element={<Sobre />}/>
+        <Route path='/Feed' element={<FeedDenuncia/>} />
+        <Route path='/Registro' element={<Registro />}/>
+
+        {/*ADM */}
+
+        <Route path='/adm/TelaInicial' element={<TelaInicialADM/>}/>
+        <Route path='/adm/Dashboard' element={<DashboardADM/>}/>
+        <Route path='/adm/FeedDenuncias' element={<FeedDenunciasADM/>}/>
+        
+        {/* Users */}
+
+        <Route path='/users/TeleInicial' element={<TelaInicialUser/>}/>
+        <Route path='/users/MeusDados' element={<MeusDados />}/>
+        <Route path='/users/notificacoes' element={<Notificacoes/>}/>
+        <Route path='/users/MinhasDenuncias' element={<MinhasDenuncias/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- Fala-Votorantim/src/main.tsx
+++ Fala-Votorantim/src/main.tsx
@@ -1,13 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css'; // Seus estilos globais
 
-
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App/>}/>
-    </Routes>
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
