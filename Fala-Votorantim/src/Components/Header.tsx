452ce8172d1f496743bc260abc9439e575b47790
César Henrique <cesar_henriquecam@hotmail.com>
2026-09-11 21:28:58 -0300

    export das páginas de usuário

--- Fala-Votorantim/src/Components/Header.tsx
+++ Fala-Votorantim/src/Components/Header.tsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export default function Header() {
     return(
         <header className="w-full bg-linear-to-r from-[#1447E6] to-[#193CB8] min-w-full ">
@@ -7,11 +9,11 @@ export default function Header() {
                     <h1>Fala Votorantim</h1>
                 </div>
                 <div className="flex items-center gap-8">
-                    <a href="/" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Início</a>
-                    <a href="/Sobre" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Sobre Nós</a>
-                    <a href="/Feed" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Feed de Denúncias</a>
-                    <a href="/Login" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Login</a>
-                    <a href="/Cadastrar" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Cadastrar</a>
+                    <Link to="/" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Início</Link>
+                    <Link to="/Sobre" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Sobre Nós</Link>
+                    <Link to="/Feed" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Feed de Denúncias</Link>
+                    <Link to="/Login" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Login</Link>
+                    <Link to="/Cadastrar" className="flex h-10 items-center justify-center bg-[#1447E6] p-5 rounded-md">Cadastrar</Link>
                 </div>
             </div>
         </header>
